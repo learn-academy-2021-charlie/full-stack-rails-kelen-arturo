8dd2adcb8147ea06832ab7aa254286b311157b1c
kel-man <kyyafuso@gmail.com>
2021-08-11 12:40:58 -0700

    restructures controller/routes for convention and fixes app navigation and database interaction

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,55 @@
+class BlogsController < ApplicationController
+  def index
+    @blogs = Blog.all
+    render 'main.html.erb'
+  end
+
+  def main
+    @blogs = Blog.all
+    render 'main.html.erb'
+  end
+
+  def show
+    @blog = Blog.find(params[:id])
+    render 'blog_show.html.erb'
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+      @blog = Blog.create(blog_params)
+      if @blog.valid?
+        redirect_to '/'
+      else
+        redirect_to '/blogs/new'
+      end
+  end
+
+  def destroy
+    @blog = Blog.find(params[:id])
+    @blog.destroy
+    redirect_to '/'
+  end
+
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+
+  def update
+    @blog = Blog.find(params[:id])
+    @blog.update(blog_params)
+    if @blog.valid?
+      redirect_to blogs_path
+    else
+      redirect_to edit_blog_path(@blog)
+    end
+  end
+
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get '/' => 'main#main', as: 'main'
-  get '/blogs/new' => 'main#new', as: 'new'
-  post '/blogs' => 'main#create', as: 'blog'
-  delete '/blogs/:id' => 'main#destroy', as: 'delete'
-  get '/blogs/:id/edit' => 'main#edit', as: 'edit'
-  patch '/blogs/:id' => 'main#update', as: 'update'
-  get '/blogs/:id' => 'main#show'
+  get '/' => 'blogs#main', as: 'blogs'
+  get '/blogs/new' => 'blogs#new', as: 'new_blog'
+  post '/blogs' => 'blogs#create', as: 'create_blog'
+  delete '/blogs:id' => 'blogs#destroy', as: 'delete_blog'
+  get '/blogs/:id/edit' => 'blogs#edit', as: 'edit_blog'
+  patch '/blogs/:id' => 'blogs#update'
+  get '/blogs/:id' => 'blogs#show'
+  # resources :blogs, only: [:index, :show, :edit, :new, :create, :update, :destroy]
+  root 'blogs#index'
 end
