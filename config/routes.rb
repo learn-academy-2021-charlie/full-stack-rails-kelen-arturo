29293cd597809c5a8076fa6b30f5f5d6a92a10c9
kel-man <kyyafuso@gmail.com>
2021-08-11 11:08:42 -0700

    adds delete button on blog_show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get '/' => 'main#main', as: 'main'
   get '/blogs/new' => 'main#new', as: 'new'
   post '/blogs' => 'main#create'
+  delete '/blogs/:id' => 'main#destroy', as: 'delete'
   get '/blogs/:id' => 'main#show'
 end
